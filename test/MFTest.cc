4bce52d5719b2567712e889bb80585798bcf8b11
qlu <qlu@e83639c7-b27d-0410-b729-efaafefb1b97>
2010-02-18 19:42:13 +0000

    Fixed compile erros due to DDS pre-compilation commands in CMakeLists.txt
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/messagefacility-svn/trunk@13 e83639c7-b27d-0410-b729-efaafefb1b97

--- test/MFTest.cc
+++ test/MFTest.cc
@@ -1,8 +1,7 @@
-//#define ML_DEBUG
+#define NDEBUG
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <boost/shared_ptr.hpp>
 
 #include <pthread.h>
@@ -13,30 +12,35 @@ using namespace mf;
 
 void * anotherLogger(void *arg)
 {
+  // Set module name
   SetModuleName("anotherLogger");
 
-  LogWarning("warn1 | warn2")<<"Followed by a WARNING message.";
-  LogDebug("debug")<<"The debug message in the other thread";
+  LogWarning("warn1 | warn2") << "Followed by a WARNING message.";
+  LogDebug("debug")           << "The debug message in the other thread";
 
   return NULL;
 }
 
 int main()
 {
-
+  // Start MessageFacility Service
   boost::shared_ptr<Presence> MFPresence;
   StartMessageFacility("MessageServicePresence", MFPresence);
+
+  // Set module name for main thread
   SetModuleName("MFTest");
 
   // Start up another logger in a seperate thread
   pthread_t tid;
   pthread_create(&tid, NULL, anotherLogger, NULL);
-  pthread_join  (tid, 0);
 
   // Issue messages with different severity levels
-  LogError("err1|err2")<<"This is an ERROR message.";
-  LogWarning("warning")<<"Followed by a WARNING message.";
-  LogDebug("debug")<<"DEBUG information.";
+  LogError("err1|err2") << "This is an ERROR message.";
+  LogWarning("warning") << "Followed by a WARNING message.";
+  LogDebug("debug")     << "DEBUG information.";
+
+  // Thread join
+  pthread_join  (tid, 0);
 
   return 0;
 }
