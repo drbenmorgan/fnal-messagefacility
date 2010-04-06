f6fb70605aa4609c8e4d775d92f334471aea8cd0
qlu <qlu@e83639c7-b27d-0410-b729-efaafefb1b97>
2010-04-06 19:51:59 +0000

    Added cross-compilatioin support in CMake for Nova
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/messagefacility-svn/trunk@66 e83639c7-b27d-0410-b729-efaafefb1b97

---
+++ xcompile/novaLinuxPPC.cmake
@@ -0,0 +1,10 @@
+SET(CMAKE_SYSTEM_NAME LinuxPPC)
+
+SET(CMAKE_C_COMPILER   /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-g++)
+
+#SET(CMAKE_FIND_ROOT_PATH /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/powerpc-603-linux-gnu)
+
+#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
