1546d676ff6d4a1978a6ea93d55f41b789e53022
qlu <qlu@e83639c7-b27d-0410-b729-efaafefb1b97>
2010-04-06 22:34:21 +0000

    1. Corrected xcompile parameters in CMake file 2. Reduced shared library dependence on dds
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/messagefacility-svn/trunk@68 e83639c7-b27d-0410-b729-efaafefb1b97

--- xcompile/novaLinuxPPC.cmake
+++ xcompile/novaLinuxPPC.cmake
@@ -1,7 +1,10 @@
-SET(CMAKE_SYSTEM_NAME LinuxPPC)
+SET (CMAKE_SYSTEM_NAME Linux)
+SET (CMAKE_SYSTEM_PROCESSOR PPC)
 
-SET(CMAKE_C_COMPILER   /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-g++)
+SET (CMAKE_C_COMPILER  
+    /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER 
+    /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/bin/powerpc-603-linux-gnu-g++)
 
 #SET(CMAKE_FIND_ROOT_PATH /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/powerpc-603-linux-gnu)
 
