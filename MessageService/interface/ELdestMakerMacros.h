92d101dfdb26956231c3af9c8ee7f139103359cc
qlu <qlu@e83639c7-b27d-0410-b729-efaafefb1b97>
2010-09-02 21:04:47 +0000

    Change ParameterSet namespace left over in factory maker macro
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/messagefacility-svn/trunk@133 e83639c7-b27d-0410-b729-efaafefb1b97

--- MessageService/interface/ELdestMakerMacros.h
+++ MessageService/interface/ELdestMakerMacros.h
@@ -1,8 +1,10 @@
 #include <string>
 
-namespace mf {
+namespace fhicl {
+  class ParameterSet;
+}
 
-class ParameterSet;
+namespace mf {
 
 namespace service {
 
@@ -12,7 +14,7 @@ class ELdestinationFactory;
 struct DestinationMaker
 {
   DestinationMaker( std::string const & type_str, 
-      ELdestination* (*f)(std::string const &, ParameterSet const &) )
+      ELdestination* (*f)(std::string const &, fhicl::ParameterSet const &) )
   {
     ELdestinationFactory::reg(type_str, f);
   }
@@ -24,7 +26,7 @@ struct DestinationMaker
 #define REG_DESTINATION(type_str, type_name)            \
   ELdestination * type_name ## _maker_func(             \
         std::string const & name,                       \
-        ParameterSet const & pset )                     \
+        fhicl::ParameterSet const & pset )              \
     { return new type_name(name, pset); }               \
   DestinationMaker type_name ## _maker_func_global_var( \
         #type_str,                                      \
