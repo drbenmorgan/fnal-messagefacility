4b5f012222672ce484aa7f90a6fc7c0c35e189b2
Qiming Lu <qlu@fnal.gov>
2011-02-21 17:01:44 -0600

    remove redundant xcompile toolchain file

--- xcompile/LinuxPPC.cmake
+++ xcompile/LinuxPPC.cmake
@@ -1,15 +1,5 @@
 SET (CMAKE_SYSTEM_NAME Linux)
-SET (CMAKE_SYSTEM_PROCESSOR PPC)
+SET (CMAKE_SYSTEM_PROCESSOR ppc)
 
-SET (CMAKE_C_COMPILER  
-#   /fnal/ups/prd/nova_crosscompiler/v4_1_2-2_3_6/bin/powerpc-603-linux-gnu-gcc)
-    powerpc-603-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER 
-#   /fnal/ups/prd/nova_crosscompiler/v4_1_2-2_3_6/bin/powerpc-603-linux-gnu-g++)
-    powerpc-603-linux-gnu-g++)
-
-#SET(CMAKE_FIND_ROOT_PATH /fnal/ups/prd/nova_crosscompiler/v4_1_0-2_3_6/powerpc-603-linux-gnu)
-
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET (CMAKE_C_COMPILER   powerpc-603-linux-gnu-gcc)
+SET (CMAKE_CXX_COMPILER powerpc-603-linux-gnu-g++)
