cecbbfca53710eece1b1dbf3652843e172c8d1fd
Kyle Knoepfel <knoepfel@fnal.gov>
2015-01-27 10:16:06 -0600

    Replace ref-qualified << overloads with decltype(auto)/std::forward pattern

--- messagefacility/MessageLogger/MessageLogger.h
+++ messagefacility/MessageLogger/MessageLogger.h
@@ -122,27 +122,23 @@ namespace mf {
 
     // &-qualified ostream operators
     template< class T >
-    MaybeLogger_ & operator << ( T const & t ) & {
+    decltype(auto) operator << ( T const & t )
+    {
       if ( !CONDITIONAL || msgSender_p.get() ) *msgSender_p << t;
-      return *this;
+      return std::forward<MaybeLogger_>(*this);
     }
 
-    MaybeLogger_ & operator << ( std::ostream&(*f)(std::ostream&) ) & {
+    decltype(auto) operator << ( std::ostream&(*f)(std::ostream&) )
+    {
       if ( !CONDITIONAL || msgSender_p.get() ) *msgSender_p << f;
-      return *this;
+      return std::forward<MaybeLogger_>(*this);
     }
 
-    MaybeLogger_ & operator << ( std::ios_base&(*f)(std::ios_base&) ) & {
+    decltype(auto) operator << ( std::ios_base&(*f)(std::ios_base&) )
+    {
       if ( !CONDITIONAL || msgSender_p.get() ) *msgSender_p << f;
-      return *this;
+      return std::forward<MaybeLogger_>(*this);
     }
-
-    // &&-qualified ostream operators
-    template< class T >
-    MaybeLogger_ && operator << ( T const & t ) && { return std::move(*this << t); }
-
-    MaybeLogger_ && operator << ( std::ostream& (*f)(std::ostream&)  ) && { return std::move(*this << f); }
-    MaybeLogger_ && operator << ( std::ios_base&(*f)(std::ios_base&) ) && { return std::move(*this << f); }
 
   };
 
