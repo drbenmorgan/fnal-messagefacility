34b0f59047e9ad1d390a729221480006f23d53f7
Kyle Knoepfel <knoepfel@fnal.gov>
2016-06-27 08:58:35 -0500

    Clean up c'tor of MessageLoggerImpl.

--- messagefacility/MessageLogger/MessageLoggerImpl.cc
+++ messagefacility/MessageLogger/MessageLoggerImpl.cc
@@ -55,9 +55,6 @@
 //16 mf   9/8/09        Eliminate caching by descrptor address during ctor
 //                      phases (since addresses are not yet permanent then)
 
-// system include files
-// user include files
-
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/JobReport.h"
 #include "messagefacility/MessageLogger/MessageLoggerImpl.h"
@@ -75,44 +72,33 @@ namespace mf {
 
   // constructors and destructor
   //
-  mf::MessageLoggerImpl::
-  MessageLoggerImpl(fhicl::ParameterSet const& iPS)
+  mf::MessageLoggerImpl::MessageLoggerImpl(fhicl::ParameterSet const& pset)
     : debugEnabled_{false}
     , messageServicePSetHasBeenValidated_{false}
     , messageServicePSetValidatationResults_{}
     , anyDebugEnabled_{false}
-    , fjrSummaryRequested_{iPS.get<bool>("messageSummaryToJobReport", false)}
+    , fjrSummaryRequested_{pset.get<bool>("messageSummaryToJobReport", false)}
   {
-    auto const& debugModules = iPS.get<std::vector<std::string>>("debugModules", {});
-    auto const& suppressDebug = iPS.get<std::vector<std::string>>("suppressDebug", {});
-    auto const& suppressInfo = iPS.get<std::vector<std::string>>("suppressInfo", {});
-    auto const& suppressWarning = iPS.get<std::vector<std::string>>("suppressWarning", {});
+    using modules_t = std::vector<std::string>;
+    auto const& debugModules = pset.get<modules_t>("debugModules", {});
+    auto const& suppressDebug = pset.get<modules_t>("suppressDebug", {});
+    auto const& suppressInfo = pset.get<modules_t>("suppressInfo", {});
+    auto const& suppressWarning = pset.get<modules_t>("suppressWarning", {});
 
     // Use these lists to prepare a map to use in tracking suppression
-
-    // Do suppressDebug first and suppressWarning last to get proper order
-    for (auto const& mod : suppressDebug) {
-      suppression_levels_[mod] = ELseverityLevel::ELsev_success;
-    }
-
-    for (auto const& mod : suppressInfo) {
-      suppression_levels_[mod] = ELseverityLevel::ELsev_info;
-    }
-
-    for (auto const& mod : suppressWarning) {
-      suppression_levels_[mod] = ELseverityLevel::ELsev_warning;
-    }
+    // .. (Do suppressDebug first and suppressWarning last to get
+    // .. proper order [FIXME -- what's this about?])
+    auto& levels = suppression_levels_;
+    cet::for_all(suppressDebug  , [&levels](auto const& mod){ levels[mod] = ELseverityLevel::ELsev_success; });
+    cet::for_all(suppressInfo   , [&levels](auto const& mod){ levels[mod] = ELseverityLevel::ELsev_info; });
+    cet::for_all(suppressWarning, [&levels](auto const& mod){ levels[mod] = ELseverityLevel::ELsev_warning; });
 
     // set up for tracking whether current module is debug-enabled
     // (and info-enabled and warning-enabled)
-    if (debugModules.empty()) {
-      anyDebugEnabled_ = false;
-      MessageDrop::instance()->debugEnabled = false;
-    } else {
-      anyDebugEnabled_ = true;
-      MessageDrop::instance()->debugEnabled = false;
-      // this will be over-ridden when specific modules are entered
-    }
+    anyDebugEnabled_ = !debugModules.empty();
+
+    // the following will be over-ridden when specific modules are entered
+    MessageDrop::instance()->debugEnabled = false;
 
     for(auto const& mod : debugModules) {
       if (mod == "*") {
@@ -127,30 +113,28 @@ namespace mf {
       MessageLoggerQ::MLqJOB(new std::string{jr_name});
     }
 
-    std::string const& jm = mf::MessageDrop::instance()->jobMode;
-    MessageLoggerQ::MLqMOD(new std::string{jm});
-
-    MessageLoggerQ::MLqCFG(new fhicl::ParameterSet{iPS});
+    MessageLoggerQ::MLqCFG(new fhicl::ParameterSet{pset});
 
   } // ctor
 
   MessageLoggerImpl::EnabledState
-  MessageLoggerImpl::setContext(std::string const &currentPhase) {
+  MessageLoggerImpl::setContext(std::string const &currentPhase)
+  {
     return setContext(currentPhase, currentPhase);
   }
 
   void
-  MessageLoggerImpl::setMinimalContext(std::string const &currentPhase) {
+  MessageLoggerImpl::setMinimalContext(std::string const &currentPhase)
+  {
     MessageDrop::instance()->moduleName = currentPhase;
   }
 
   MessageLoggerImpl::EnabledState
-  MessageLoggerImpl::setContext(std::string const &currentProgramState,
-                                std::string const &levelsConfigLabel) {
-    MessageDrop *md = MessageDrop::instance();
-    EnabledState previousState(md->debugEnabled,
-                               md->infoEnabled,
-                               md->warningEnabled);
+  MessageLoggerImpl::setContext(std::string const& currentProgramState,
+                                std::string const& levelsConfigLabel)
+  {
+    auto md = MessageDrop::instance();
+    EnabledState const previousState {md->debugEnabled, md->infoEnabled, md->warningEnabled};
     md->moduleName = currentProgramState;
 
     if (!anyDebugEnabled_) {
@@ -158,9 +142,8 @@ namespace mf {
     } else if (everyDebugEnabled_) {
       md->debugEnabled = true;
     } else {
-      md->debugEnabled =
-        debugEnabledModules_.count(levelsConfigLabel);
-      s_map_t::const_iterator it = suppression_levels_.find(levelsConfigLabel);
+      md->debugEnabled = debugEnabledModules_.count(levelsConfigLabel);
+      auto it = suppression_levels_.find(levelsConfigLabel);
       if (it != suppression_levels_.end()) {
         md->debugEnabled = md->debugEnabled && (it->second < ELseverityLevel::ELsev_success);
         md->infoEnabled = (it->second < ELseverityLevel::ELsev_info);
@@ -175,8 +158,9 @@ namespace mf {
 
   void
   MessageLoggerImpl::setContext(std::string const &currentPhase,
-                                EnabledState previousEnabledState) {
-    MessageDrop* md = MessageDrop::instance();
+                                EnabledState previousEnabledState)
+  {
+    auto md = MessageDrop::instance();
     md->moduleName = currentPhase;
     md->debugEnabled = previousEnabledState.debugEnabled();
     md->infoEnabled = previousEnabledState.infoEnabled();
