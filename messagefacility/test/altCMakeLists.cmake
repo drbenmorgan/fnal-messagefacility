ac569ca1ee363d6feba1e19099023441b67369bc
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-07-28 17:28:59 +0100

    Update tests for altCMake and environment settings

--- messagefacility/test/altCMakeLists.cmake
+++ messagefacility/test/altCMakeLists.cmake
@@ -1,7 +1,3 @@
-if(ALT_CMAKE)
-  include(altCMakeLists.cmake)
-else()
-
 # ======================================================================
 #
 # Testing
@@ -11,10 +7,16 @@ else()
 cet_enable_asserts()
 include(CetTest)
 
+# - General Test environment:
 cet_test_env("FHICL_FILE_PATH=.")
+
+# - Dynamic loader path
+set(SYSTEM_LD_LIBRARY_PATH LD_LIBRARY_PATH)
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+  set(SYSTEM_LD_LIBRARY_PATH DYLD_LIBRARY_PATH)
+endif()
+cet_test_env("${SYSTEM_LD_LIBRARY_PATH}=$<TARGET_FILE_DIR:MF_Utilities>:$ENV{${SYSTEM_LD_LIBRARY_PATH}}")
 
 add_subdirectory(Utilities)
 add_subdirectory(Integration)
 add_subdirectory(MessageService)
-
-endif()
