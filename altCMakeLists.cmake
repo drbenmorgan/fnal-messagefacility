4620c59dfd6ecaf009b52c356029685f87bc9be3
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-06-18 16:25:10 +0100

    Bump altCMake version to 2.2.2

--- altCMakeLists.cmake
+++ altCMakeLists.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.3)
-project(messagefacility VERSION 2.2.0)
+project(messagefacility VERSION 2.2.2)
 
 # cetbuildtools contains our cmake modules
 # - Cetbuildtools, version2
